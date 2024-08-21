5085a65f8ed96ddd0b59ea2fb3c3fc00d4c76b78
wachin <wachin.id@gmail.com>
2024-08-21 11:27:08 -0500

    Actualización mié 21 ago 2024 11:27:08 -05

---
+++ Script nnn open auto-push-script-3.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#!/bin/sh
+# Verifica si ya hay una instancia de Konsole abierta
+KONSOLE_PID=$(pgrep -x konsole)
+if [ -n "$KONSOLE_PID" ]; then
+    # Si hay una instancia abierta, ejecuta el script en esa ventana
+    konsole -p tabtitle='PFSLE' -e /bin/bash "auto-push-script-2.sh"
+else
+    # Si no hay una instancia abierta, abre una nueva ventana de Konsole
+    konsole -p tabtitle='PFSLE' -e /bin/bash "auto-push-script-2.sh"
+fi
+
+# gnome-terminal --tab --title="PFSLE" --command="bash -c 'nnn auto-push-script.sh; $SHELL'"
+
+## Requerimientos, tener instalado clex y gnome-terminal y abierta la terminal gnome
+## sudo apt install clex gnome-terminal
+## La primera vez que se abra clex debe configurarlo, sólo lea las instrucciones y acepte.
+## Script basado en:
+## How can I make a script that opens terminal windows and executes commands in them?
+## https://askubuntu.com/questions/46627/how-can-i-make-a-script-that-opens-terminal-windows-and-executes-commands-in-the
+## https://askubuntu.com/a/1105741/145772
+# Here's a breakdown of that command above:
+# gnome-terminal --tab --title="tab 1" --command="bash -c 'cd /etc; ls; $SHELL'" --tab --title="tab 2" --command="bash -c 'cd ~; ls; $SHELL'" --tab --title="tab 3" --command="bash -c 'cd ~/temp3; ls; $SHELL'"
+# Nota: Ese comando abre 3 pestañas.
+# gnome-terminal = open up a gnome-terminal
+# --tab = open up a unique tab for what comes next
+# --title="tab 1" = title this tab "tab 1"
+# --command="bash -c 'cd /etc; ls; $SHELL'" = run the bash -c 'cd /etc; ls; $SHELL' command, which is a command I just made up as an example; here's # what it does:
+# bash -c says it is a bash 'c'ommand
+# cd /etc = 'c'hange 'd'irectory into the "/etc" path
+# ls = 'l'i's't contents of this directory
+# $SHELL = this cryptic tidbit is required to keep the shell open so you can work with it. If you want the shell to open, run your command, then close, simply remove this part. I, however, want the tab to stay open so I can go make programming magic. :)
+# we then start back over at the --tab part to produce tab 2, then again for tab 3. Customize to your heart's content.
